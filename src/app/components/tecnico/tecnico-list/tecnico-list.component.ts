e5c37de5e8e2590c6c99dc198aedc8072c35a373
andremendoncaolv <andremendoncaolv@gmail.com>
2023-09-12 17:17:06 -0300

    HTML e CSS para o componente TECNICO-LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
 })
 export class TecnicoListComponent {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Andre Oliveira',
+      cpf: '123.456.789-10',
+      email: 'andre@email.com',
+      senha: '12333',
+      perfis: ['0'],
+      dataCriacao: '13/09/2008'
+    }
+  ]
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
